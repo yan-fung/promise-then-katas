f9bff0d73990331fe5dfbdb682393823cb1a098f
Romy Greenfield <rgreenfield@adaptavist.com>
2020-12-15 20:59:28 +0000

    renamed getData references to refer to fetch

--- src/promise-katas-2.js
+++ src/promise-katas-2.js
@@ -22,22 +22,22 @@ fetch("joke", "question").then(<insert your callback function>)
 
 */
 
-// 1 Create a function that uses the getData function to make a request to the "food" URL and returns
+// 1 Create a function that uses the fetch function to make a request to the "food" URL and returns
 // the data - expected return value "Cheese" of type String
 
 const food = () => {};
 
-// 2 Create a function that uses the getData function to make a request to the "cats" URL and returns
+// 2 Create a function that uses the fetch function to make a request to the "cats" URL and returns
 // a list of cats in alphabetical order - expected return value ["Bandit", "Berry", "Puss in boots", "Smokey"] of type Array
 
 const cat = () => {};
 
-// 3 Create a function that uses the getData function to make a request to the "dogs" URL and returns
+// 3 Create a function that uses the fetch function to make a request to the "dogs" URL and returns
 // the naughtiest dog - expected return value {name: "Mutley", naughty: 10} of type Object
 
 const dog = () => {};
 
-// 4 Create a function that uses the getData function to make requests to the "jokes" URL and returns
+// 4 Create a function that uses the fetch function to make requests to the "jokes" URL and returns
 // a joke object with the key of question and answer - expected return { 
 //     question: "Why did the scarecrow win the Nobel Prize?",
 //     answer: "Because he was out-standing in his field."
